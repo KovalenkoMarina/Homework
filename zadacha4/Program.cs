a574bf6a24ab2fa436542b28182f050e21521a6c
Marina <kovalenko_marina@inbox.ru>
2022-05-08 13:06:27 +0700

    вывод всех четных чисел

---
+++ zadacha4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int n = 1;
+while (n<=a)
+{
+    if (n%2 ==0)
+    {
+        Console.Write(" " + n); 
+    }
+    else 
+    {
+        Console.Write(" "); 
+    }
+    n++;
+}
+
+
+
