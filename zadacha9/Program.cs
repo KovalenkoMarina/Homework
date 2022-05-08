d967f20b6de7fa21e54bc3b9cc0fcaa4d9f8767a
Marina <kovalenko_marina@inbox.ru>
2022-05-08 14:53:07 +0700

    третья цифра

---
+++ zadacha9/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+
+if (n>99)
+{
+    int x = n; 
+    while (x >= 1000)
+    {
+        x = x / 10;
+    }
+    Console.WriteLine("Третья цифра ");
+    Console.Write(x%10);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
