a133cd2914c730ab2f1320aa7abe536f0ea255bc
Marina <kovalenko_marina@inbox.ru>
2022-05-08 12:57:07 +0700

    Определение четности

---
+++ zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+
+if (a%2 == 0)
+{
+    Console.Write("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
