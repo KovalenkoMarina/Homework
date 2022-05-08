dd96878b331a6c833c15659165bf761a387b4ad9
Marina <kovalenko_marina@inbox.ru>
2022-05-08 13:21:16 +0700

    наибольшая цифра двузначного числа

---
+++ zadacha6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int a = n/10;
+int b = n%10;
+Console.WriteLine("Наибольшая цифра ");
+if (a>b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
