c8f5cbcf15fd2bc4cfa557bc95ca49383cc173e7
Marina <kovalenko_marina@inbox.ru>
2022-05-08 13:12:35 +0700

    вторая цифра трехзначного числа

---
+++ zadacha5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+if (n>99 && n<1000)
+{
+    Console.WriteLine("Вторая цифра ");
+    Console.Write((n%100)/10);
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
