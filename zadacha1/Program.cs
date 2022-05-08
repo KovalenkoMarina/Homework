693ec77be29c506b8e0ba88b5a048a51d4ba85ac
Marina <kovalenko_marina@inbox.ru>
2022-05-08 08:57:09 +0700

    квадрат числа

---
+++ zadacha1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1 ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b)
+{
+    Console.Write("YES");
+}
+else
+{
+    Console.WriteLine("No");
+}
