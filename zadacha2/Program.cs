102f5a030fade4e03c55d5a87abd2ba5754b4b40
Marina <kovalenko_marina@inbox.ru>
2022-05-08 09:07:39 +0700

    дни недели

---
+++ zadacha2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int a = int.Parse(Console.ReadLine());
+
+if (a == 1)
+{
+    Console.Write("Понедельник");
+}
+if (a == 2)
+{
+    Console.Write("Вторник");
+}
+if (a == 3)
+{
+    Console.Write("Среда");
+}
+if (a == 4)
+{
+    Console.Write("Четверг");
+}
+if (a == 5)
+{
+    Console.Write("Пятница");
+}
+if (a == 6)
+{
+    Console.Write("Суббота");
+}
+if (a == 7)
+{
+    Console.Write("Воскресенье");
+}
+
