ac2df21e4f43c5eb40b38549d635d12f34c9216d
Marina <kovalenko_marina@inbox.ru>
2022-05-08 14:20:49 +0700

    проверяем кратность

---
+++ zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите делимое ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите делитель ");
+int b = int.Parse(Console.ReadLine());
+int c = a%b;
+if (c == 0)
+{
+    Console.WriteLine("Число кратно заданному");
+}
+else
+{
+    Console.WriteLine("Число некратно заданному, остаток = " +c);
+}
