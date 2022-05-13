e8682844692e5464e6f32f15c44f4fe10e8784f5
Marina <kovalenko_marina@inbox.ru>
2022-05-13 09:13:34 +0700

    определяем является ли одно число квадратом другого

---
+++ zadacha11/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число 1 ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2 ");
+int b = int.Parse(Console.ReadLine());
+if (a == b*b || b == a*a)
+{
+    Console.Write("YES");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
