28b38870960e2a78002abd7119b9df7e4b7308a7
Marina <kovalenko_marina@inbox.ru>
2022-05-13 10:35:15 +0700

    расстояние между точками на плоскости

--- zadacha13/Program.cs
+++ zadacha13/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите х1");
-int x1 = int.Parse(Console.ReadLine());
+float x1 = float.Parse(Console.ReadLine());
 Console.WriteLine("Введите у1 ");
-int y1 = int.Parse(Console.ReadLine());
+float y1 = float.Parse(Console.ReadLine());
 Console.WriteLine("Введите х2");
-int x2 = int.Parse(Console.ReadLine());
+float x2 = float.Parse(Console.ReadLine());
 Console.WriteLine("Введите у2 ");
-int y2 = int.Parse(Console.ReadLine());
-double result = Convert.ToInt32(Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)));
+float y2 = float.Parse(Console.ReadLine());
+double result = Math.Sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
 Console.WriteLine("Расстояние между точками " + result);
 
