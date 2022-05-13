7ff0173978100379eb1fa278e741d764c684ec0d
Marina <kovalenko_marina@inbox.ru>
2022-05-13 09:27:27 +0700

    определение диапазона координат по номеру плоскости

---
+++ zadacha12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите номер четверти координатной плоскости ");
+int quarter = int.Parse(Console.ReadLine());
+
+if (quarter == 1)
+{
+    Console.Write("x > 0, y > 0");
+}
+if (quarter == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+if (quarter == 3)
+{
+    Console.Write("x < 0, y < 0");
+}
+if (quarter == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+
+
