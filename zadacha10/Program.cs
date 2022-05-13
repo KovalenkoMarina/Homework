eee250069783e46e252dcc14a0b0a8ef6744302e
Marina <kovalenko_marina@inbox.ru>
2022-05-13 09:09:49 +0700

    определение выходного по номеру дня недели

---
+++ zadacha10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите номер дня недели ");
+int number_day = int.Parse(Console.ReadLine());
+if (number_day == 6 || number_day == 7)
+{
+    Console.Write("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
+}
+
