177053c66a1ae790082bb560f609fa4f27305ce6
Marina <kovalenko_marina@inbox.ru>
2022-05-21 14:47:45 +0700

    Таблица кубов от 1 до N

---
+++ zadacha16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Таблица кубов чисел от 1 до {n} ");
+int a = 1;
+while (a <= n)
+{
+    int b = a*a*a;
+    Console.WriteLine(a + " " + b);
+    a++;
+}
