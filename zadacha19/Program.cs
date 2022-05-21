26eeddf260d4c63d2d736d6a001c8bd679642c85
Marina <kovalenko_marina@inbox.ru>
2022-05-21 15:10:10 +0700

    сумма цифр в числе

---
+++ zadacha19/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int number = 0;
+int sum = 0;
+while (n > 0)
+{
+    number = n%10;
+    sum = sum + number;
+    n = n/10;
+}
+Console.WriteLine($"Сумма в числе {sum}");
