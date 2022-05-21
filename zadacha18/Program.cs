40f9f53bd47c6199e3e180774b3fbb914062bfa5
Marina <kovalenko_marina@inbox.ru>
2022-05-21 15:00:40 +0700

    Возведите число А в натуральную степень B используя цикл

---
+++ zadacha18/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите степень ");
+int b = int.Parse(Console.ReadLine());
+int n = 1;
+int result = 1;
+while (n <= b)
+{
+    result = result*a;
+    n++;
+}
+Console.Write(result);
