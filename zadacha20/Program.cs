3efc4d0c6018a932b70645f27698d68dda33d402
Marina <kovalenko_marina@inbox.ru>
2022-05-21 15:27:44 +0700

    кубы чисел, заканчивающиеся на четную цифру

---
+++ zadacha20/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Кубы чисел от 1 до {n}, заканчивающиеся на четную цифру");
+int a = 1;
+while (a <= n)
+{
+    int b = a*a*a;
+    if (b%2 == 0)
+    {
+        Console.WriteLine(b + " "); 
+    }
+        a++;
+}
