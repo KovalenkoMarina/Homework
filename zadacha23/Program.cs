36a05cdaa95c141e66008925e0fca77d3b102fb0
Marina <kovalenko_marina@inbox.ru>
2022-05-22 16:33:14 +0700

    рандомный массив, количество четных и нечетных чисел

---
+++ zadacha23/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задать массив, заполнить случайными положительными трёхзначными числами.
+// Показать количество нечетных\четных чисел
+int[] array = new int[10];
+int count1 = 0;
+int count2 = 0;
+for (int i = 0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    Console.Write($"{array[i]}  ");
+    if (array[i]%2==0)
+    {
+        count1++;
+    }
+    else
+    {
+        count2++;
+    }
+    
+}
+Console.WriteLine();
+Console.WriteLine("количество четных чисел " + count1);
+Console.WriteLine("количество нечетных чисел " + count2);
