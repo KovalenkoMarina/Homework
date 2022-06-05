48ebaffac4c706f38a132765cf6a7f742c7b9cfa
Marina <kovalenko_marina@inbox.ru>
2022-06-05 14:45:29 +0700

    треугольник паскаля

---
+++ zadacha40/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+Console.Write("Введите число элементов в основании треугольника Паскаля ");
+int n = int.Parse(Console.ReadLine() ?? "");
+int i, c;
+float factorial(int n)
+{
+    float i, x = 1;
+    for (i = 1; i <= n; i++)
+        x *= i;
+    return x;
+    }
+for (i = 0; i < n; i++)
+{
+    for (c = 0; c <= (n - i); c++) 
+        Console.Write(" ");
+    for (c = 0; c <= i; c++)
+    {
+        Console.Write(" ");
+        Console.Write(factorial(i) / (factorial(c) * factorial(i - c))); 
+    }
+    Console.WriteLine();
+    Console.WriteLine(); 
+}
+Console.ReadLine();
+ 
