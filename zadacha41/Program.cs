eaffa2b451ce1a560c665cf670ed3250953cdaa5
Marina <kovalenko_marina@inbox.ru>
2022-06-07 08:49:45 +0700

    Показать натуральные числа от M до N, N и M заданы

---
+++ zadacha41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine("Введите числа N и M, N>M");
+Console.WriteLine("N = ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("M = ");
+int m = int.Parse(Console.ReadLine());
+void Recursion(int n)
+{
+    if (n < m)return;
+    {
+        Recursion(n - 1);
+        Console.Write(n + " ");
+    }
+}
+Recursion(n);
